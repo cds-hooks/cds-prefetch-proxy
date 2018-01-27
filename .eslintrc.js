9e9596cbc21c582de5556ff5ff76e177e35853dd
Matt Berther <matt@berther.io>
2018-01-27 11:46:48 -0600

    eslint cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,11 @@
-module.exports = {
-  "env": {
-    "es6": true,
-    "node": true,
-    "mocha": true
-  },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "sourceType": "module"
-  },
-  "rules": {
-    "indent": [
-      "error",
-      2
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ]
-  }
+module.exports = exports = {
+    extends: 'xo',
+    env: {
+        node: true,
+        mocha: true
+    },
+    rules: {
+        indent: ['error', 4],
+        camelcase: ['error', {properties: 'never'}]
+    }
 };
--- test/server/index.js
+++ test/server/index.js
@@ -5,70 +5,74 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-app.post('/cds-services/:serviceId/analytics/:uuid', function(req, res) {
-  if (req.params.uuid === 'error') {
-    return res.status(500).send('Internal Server Error');
-  }
-
-  res.status(204).end();
-});
-
-app.post('/cds-services/:serviceId', function(req, res) {
-  if (req.params.serviceId === '500') {
-    return res.status(500).send('Internal Server Error');
-  }
-
-  readFile(path.join('/data', req.params.serviceId + '-card.json'), function(data) {
-    if (req.params.serviceId === 'patient') { data.cards[0].detail = req.body.prefetch.patient.resource.id; }
-    res.json(data);
-  });
-});
-
-app.get('/Patient/:id', function(req, res) {
-  readFile('/data/patient-resource.json', function(data) {
-    data.resource.id = req.params.id;
-    res.json(data);
-  });
-});
-
-app.get('/cds-services', function(req, res) {
-  readFile('/data/services.json', function(data) {
-    res.json(data);
-  });
-});
-
-app.setServiceResult = function(code) {
-  var routes = app._router.stack;
-  routes.forEach(function(route, i, routes) {
-    if (route.route && route.route.path === '/cds-services') {
-      routes.splice(i, 1);
+app.post('/cds-services/:serviceId/analytics/:uuid', function (req, res) {
+    if (req.params.uuid === 'error') {
+        return res.status(500).send('Internal Server Error');
     }
-  });
 
-  if (code === 500) {
-    app.get('/cds-services', function(req, res) {
-      res.status(500).send('Internal Server Error');
-    });
-  } else if (code == 502) {
-    app.get('/cds-services', function(req, res) {
-      readFile('/data/invalid-services.json', function(data) {
+    res.status(204).end();
+});
+
+app.post('/cds-services/:serviceId', function (req, res) {
+    if (req.params.serviceId === '500') {
+        return res.status(500).send('Internal Server Error');
+    }
+
+    readFile(path.join('/data', req.params.serviceId + '-card.json'), function (data) {
+        if (req.params.serviceId === 'patient') {
+            data.cards[0].detail = req.body.prefetch.patient.resource.id;
+        }
         res.json(data);
-      });
     });
-  } else {
-    app.get('/cds-services', function(req, res) {
-      readFile('/data/services.json', function(data) {
+});
+
+app.get('/Patient/:id', function (req, res) {
+    readFile('/data/patient-resource.json', function (data) {
+        data.resource.id = req.params.id;
         res.json(data);
-      });
     });
-  }
+});
+
+app.get('/cds-services', function (req, res) {
+    readFile('/data/services.json', function (data) {
+        res.json(data);
+    });
+});
+
+app.setServiceResult = function (code) {
+    var routes = app._router.stack;
+    routes.forEach(function (route, i, routes) {
+        if (route.route && route.route.path === '/cds-services') {
+            routes.splice(i, 1);
+        }
+    });
+
+    if (code === 500) {
+        app.get('/cds-services', function (req, res) {
+            res.status(500).send('Internal Server Error');
+        });
+    } else if (code === 502) {
+        app.get('/cds-services', function (req, res) {
+            readFile('/data/invalid-services.json', function (data) {
+                res.json(data);
+            });
+        });
+    } else {
+        app.get('/cds-services', function (req, res) {
+            readFile('/data/services.json', function (data) {
+                res.json(data);
+            });
+        });
+    }
 };
 
 function readFile(file, cb) {
-  fs.readFile(path.join(__dirname, file), function(err, data) {
-    if(err) throw err;
-    return cb(JSON.parse(data));
-  });
+    fs.readFile(path.join(__dirname, file), function (err, data) {
+        if (err) {
+            throw err;
+        }
+        return cb(JSON.parse(data));
+    });
 }
 
 module.exports = app;
